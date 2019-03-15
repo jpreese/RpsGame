e03139dcc97894e19cc2df93e5df733a5a54de84
Matthew White <mwhite@plex.com>
2019-03-15 15:48:59 -0400

    Add new spec test cases

--- src/RpsGame.Tests/ProgramTests.cs
+++ src/RpsGame.Tests/ProgramTests.cs
@@ -12,23 +12,39 @@ namespace RpsGame.Tests
     public static IEnumerable<object[]> UserWins = new List<object[]>
     {
       new object[]{ThrowChoice.Rock, ThrowChoice.Scissors},
+      new object[]{ThrowChoice.Rock, ThrowChoice.Lizard},
       new object[]{ThrowChoice.Paper, ThrowChoice.Rock},
-      new object[]{ThrowChoice.Scissors, ThrowChoice.Paper}
+      new object[]{ThrowChoice.Paper, ThrowChoice.Spock},
+      new object[]{ThrowChoice.Scissors, ThrowChoice.Paper},
+      new object[]{ThrowChoice.Scissors, ThrowChoice.Lizard},
+      new object[]{ThrowChoice.Lizard, ThrowChoice.Paper},
+      new object[]{ThrowChoice.Lizard, ThrowChoice.Spock},
+      new object[]{ThrowChoice.Spock, ThrowChoice.Scissors},
+      new object[]{ThrowChoice.Spock, ThrowChoice.Rock}
     };
 
     public static IEnumerable<object[]> Ties = new List<object[]>
     {
       new object[]{ThrowChoice.Rock, ThrowChoice.Rock},
       new object[]{ThrowChoice.Paper, ThrowChoice.Paper},
-      new object[]{ThrowChoice.Scissors, ThrowChoice.Scissors}
+      new object[]{ThrowChoice.Scissors, ThrowChoice.Scissors},
+      new object[]{ThrowChoice.Lizard, ThrowChoice.Lizard},
+      new object[]{ThrowChoice.Spock, ThrowChoice.Spock}
     };
 
 
     public static IEnumerable<object[]> ComputerWins = new List<object[]>
     {
+      new object[]{ThrowChoice.Scissors, ThrowChoice.Rock},
+      new object[]{ThrowChoice.Lizard, ThrowChoice.Rock},
       new object[]{ThrowChoice.Rock, ThrowChoice.Paper},
+      new object[]{ThrowChoice.Spock, ThrowChoice.Paper},
       new object[]{ThrowChoice.Paper, ThrowChoice.Scissors},
-      new object[]{ThrowChoice.Scissors, ThrowChoice.Rock}
+      new object[]{ThrowChoice.Lizard, ThrowChoice.Scissors},
+      new object[]{ThrowChoice.Paper, ThrowChoice.Lizard},
+      new object[]{ThrowChoice.Spock, ThrowChoice.Lizard},
+      new object[]{ThrowChoice.Scissors, ThrowChoice.Spock},
+      new object[]{ThrowChoice.Rock, ThrowChoice.Spock}
     };
 
 
